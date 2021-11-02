8c74153195ba9edcdef8511c9a5e3686b228dbfd
Jorge Toledo <tolbar@Jorges-MacBook-Pro.local>
2021-11-01 23:29:33 -0500

    Adding 3 resources construct the level 2 and libraries cdk lambda and apigw

--- lib/cdk-first-app-stack.ts
+++ lib/cdk-first-app-stack.ts
@@ -1,9 +1,41 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as path from 'path';
 
 export class CdkFirstAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Dynamodb table definition
+    const greetingsTable = new dynamodb.Table(this, "GreetingsTable", {
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING
+      }
+    });
+
+    // Lambda function
+    const saveHelloFunction = new lambda.Function(this, "SaveHelloFunction",{
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handler.saveHello',
+      code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda')),
+      environment: {
+        GREETINGS_TABLE: greetingsTable.tableName
+      }
+    });
+
+    // permissions to lambda to dynamo table
+    greetingsTable.grantReadWriteData(saveHelloFunction);
+
+
+    // create THE API Gateway with one method and path
+    const helloApi = new apigw.RestApi(this,"helloApi");
+
+    helloApi.root
+      .resourceForPath("hello")
+      .addMethod("POST", new apigw.LambdaIntegration(saveHelloFunction));
+
   }
 }
